41790b6182c66162cb382bd12c6bc44884c1cf0b
jiyanx23 <jiyanxu2@illinois.edu>
2022-03-22 15:16:26 -0500

    finish something

--- IS310_assignments/humanist_scraping.py
+++ IS310_assignments/humanist_scraping.py
@@ -1,3 +1,4 @@
+from base64 import urlsafe_b64decode
 from bs4 import BeautifulSoup
 import requests
 
@@ -22,12 +23,66 @@ for link in links:
         volume_links[text] = f"{website}{url[1:]}"
         # print(volume_links[text])
 
-volume31 = volume_links["Volume 31"]
-volume32 = volume_links["Volume 32"]
-volume31_response = requests.get(volume31)
-volume32_response = requests.get(volume32)
-soup_31 = BeautifulSoup(volume31_response.text, features="html.parser")
-soup_32 = BeautifulSoup(volume32_response.text, features="html.parser")
+# for k, v in volume_links.items():
+#     print(k)
+
+
+sub_links = {}
+
+for key, value in volume_links.items():
+    link_sub = {}
+    response_sub = requests.get(value)
+    response_text_sub = response_sub.text
+    soup_sub = BeautifulSoup(response_text_sub, features="html.parser")
+    links_sub = soup_sub.find_all('a')
+    for links in links_sub:
+        text_sub = links.get_text()
+        url_sub = links.get("href")
+        # print(url_sub)
+        if "subject.html" in response_sub.url:
+            index = response_sub.url.find("subject.html")
+            new_url = response_sub.url[:index]
+            link_sub[text_sub] = f"{new_url}{url_sub}"
+            # print(f"{new_url}{url_sub}")
+        else:
+            link_sub[text_sub] = f"{value}{url_sub}"
+    sub_links[key] = link_sub
+
+# from this part is the getting two website content
+# for k,v in sub_links.items():
+#     print(k)
+
+volume1_url = sub_links["Volume 1 5/87-5/88"]["8705.1324.txt"]    
+volume1_reponse = requests.get(volume1_url)
+response_text_1 = volume1_reponse.text
+soup_1 = BeautifulSoup(response_text_1, features="html.parser")
+volume1_text = soup_1.getText()
+# print(soup_1.get_text())
+
+
+volume33_url = sub_links["Volume 33"]["May 7, 2019, 6:16 a.m. Humanist 33.1"]
+volume33_reponse = requests.get(volume33_url)
+response_text_33 = volume33_reponse.text
+soup_33 = BeautifulSoup(response_text_33, features="html.parser")
+volume33_text = soup_33.getText()
+# print(soup_33.get_text())
+
+
+
+
+
+
+
+
+
+
+
+# volume31 = volume_links["Volume 31"]
+# volume32 = volume_links["Volume 32"]
+# volume31_response = requests.get(volume31)
+# volume32_response = requests.get(volume32)
+# soup_31 = BeautifulSoup(volume31_response.text, features="html.parser")
+# soup_32 = BeautifulSoup(volume32_response.text, features="html.parser")
 # print(soup_31.prettify())
 # print(soup_32.prettify())
 
--- IS310_assignments/spring_break_hw.py
+++ IS310_assignments/spring_break_hw.py
@@ -4,27 +4,30 @@ import requests
 
 
 infile = open("raw_script_urls.txt", "r", encoding = "ISO-8859-1")
-outfile = open("scripts.txt", "w")
 urls = []
+website_name = []
 for line in infile:
-    index = line.strip().find("http")
-    url = line.strip()[index:]
-    # print(url)
-    urls.append(url)
+    # index = line.strip().find("http")
+    # url = line.strip()[index:]
+    line_split = line.strip().split(" +++$+++ ")
+    urls.append(line_split[2])
+    website_name.append(line_split[1])
+    # urls.append(url)
 
-# url = urls[0]
-# response = requests.get(url)
-# response_text = response.text
-# soup = BeautifulSoup(response_text, features="html.parser") 
-# script = soup.find_all('pre')
-# print(script)
 
-for url in urls:
-    response = requests.get(url)
-    response_text = response.text
-    soup = BeautifulSoup(response_text, features="html.parser") 
-    script = soup.find('pre').get_text()
-    outfile.write(script)
+for i in range(len(urls)):
+    try:
+        response = requests.get(urls[i])
+    except requests.ConnectionError:
+        print("connection error")      
+    else:
+        response_text = response.text
+        soup = BeautifulSoup(response_text, features="html.parser") 
+        script = soup.find('pre')
+        if not type(script) == type(None):
+            outfile = open(f"{website_name[i]}.txt", "w")
+            outfile.write(script.get_text())
+    
 
 
 infile.close()
